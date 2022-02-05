3e44768beb0069a8139af23d8eb22c26f5b7e47e
Brian Moniz <bmoniz328@gmail.com>
2022-02-05 13:40:45 -0800

    moved footer to side

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,8 +5,7 @@ export default function Footer() {
     return (
         <div className="footer-div">
             <h4>Created by Brian Moniz | 2022</h4>
-            <h5>*All convention logos and celebrity images are being used for leisure purposes only.</h5>
-            <h5>I do not own any image of any celebrity on this website.</h5>
+            <h5>*All convention logos and celebrity images are being used for leisure purposes only. I do not own any image of any celebrity used on this website.</h5>
 
         </div>
     )
