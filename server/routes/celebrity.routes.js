746870e118aec27e0c29116a968109a18972c890
Brian Moniz <bmoniz328@gmail.com>
2022-02-18 12:36:40 -0800

    added authenticate back

--- server/routes/celebrity.routes.js
+++ server/routes/celebrity.routes.js
@@ -4,7 +4,7 @@ const CelebrityController = require('../controllers/celebrity.controller');
 const { authenticate } = require("../config/jwt.config");
 
 module.exports = (app) => {
-    app.get('/api/celebrities', CelebrityController.getAllCelebrities);
+    app.get('/api/celebrities', authenticate, CelebrityController.getAllCelebrities);
     app.post('/api/celebrities', authenticate, CelebrityController.addNewCelebrity);
     app.get('/api/celebrities/:id', authenticate, CelebrityController.getOneCelebrity);
     app.put('/api/celebrities/:id', authenticate, CelebrityController.updateCelebrity);
